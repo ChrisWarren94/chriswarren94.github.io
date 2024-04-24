88ec9d49e4502d70f4d9f60fd98ebda5c13ddb85
Chris Warren <chris@wildlifeacoustics.com>
2024-04-23 21:17:50 -0400

    Implemented basic, non-responsive formatting.

--- main.js
+++ main.js
@@ -59,3 +59,17 @@ const generateTOC = () => {
 if (allHeadings.length > 0 && pageContent) {
   generateTOC();
 }
+
+
+// Removes excess indentation from beginning of codeblock elements
+[].forEach.call(document.querySelectorAll('pre'), function($pre) {
+  var lines = $pre.textContent.split('\n');
+  var matches;
+  var indentation = (matches = /^\s+/.exec(lines[0])) != null ? matches[0] : null;
+  if (!!indentation) {
+    lines = lines.map(function(line) {
+      return line.replace(indentation, '');
+    });
+    return $pre.textContent = lines.join('\n').trim();
+  }
+});
